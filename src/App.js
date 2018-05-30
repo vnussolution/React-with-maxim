c2e5be4222fe1bc349d300d66ea5d1e057749216
Truc Nguyen <vnussolution@gmail.com>
2018-05-29 18:42:54 -0700

    init setup react app with eslint prettier config

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>hello thereqwe</h2>
+
+        <h3> love asdf sdf sdfsdf</h3>
+        <Person />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const person = () => <p> I'm the Person </p>;
+
+export default person;
